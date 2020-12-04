2984f466d1970cb05200b632eb1ff8f5605ba517
linn <673879891@qq.com>
2020-12-04 21:24:21 +0800

    initial

---
+++ unziptxt.py
@@ -0,0 +1,13 @@
+import decompress 
+import argparse
+
+#添加参数
+parser = argparse.ArgumentParser()
+parser.add_argument('inputFile', help='inputFile')
+parser.add_argument('outputFile', help='ouputFile')
+args=parser.parse_args()
+print(args.inputFile)
+print(args.outputFile)
+
+# mycompress=compress.Compress()
+# mycompress.ziptxt(inputFile,outputFile);
