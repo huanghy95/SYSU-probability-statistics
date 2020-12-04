2207b72e11497364b84aa602856f7a1277137cf7
linn <673879891@qq.com>
2020-12-04 21:03:22 +0800

    initial

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import compress 
+import argparse
+
+#添加参数
+parser = argparse.ArgumentParser()
+parser.add_argument('inputFile', help='inputFile')
+parser.add_argument('outputFile', help='ouputFile')
+args=parser.parse_args()
+print(args.inputFile)
+print(args.ouputFile)
+
+# mycompress=compress.Compress()
+# mycompress.ziptxt(inputFile,outputFile);
--- compress.py
+++ compress.py
@@ -1,24 +1,29 @@
 import numpy as np
 import string
 class Compress:
+    #初始化变量
     def __init__(self):
-        self.left=0.0
-        self.right=1.0
-        self.last=" "
-        self.cnt={}
+        self.left=0.0#左区间
+        self.right=1.0#右区间
+        self.last=" "#上一个字符
+        self.cnt={}#字典
+        #初始化字典(每个出现一次,方便计算概率)
         for first in string.ascii_lowercase:
             self.cnt[first]=1
             for second in string.ascii_lowercase:
                 self.cnt[first+second]=1
-        self.lp=np.zeros(256)
-        self.rp=np.zeros(256)
+        #初始化总数
         self.total1=256
         self.total2=65536
+        #初始化概率的左右端点
+        self.lp1=np.zeros(256)
+        self.rp1=np.zeros(256)
 
+    #读取一个字符(TODO)
     def read(self,c):
         diff=self.right-self.left
-        self.right=self.left+diff*self.rp[c]
-        self.left=self.left+diff*self.lp[c]
+        self.right=self.left+diff*self.rp1[c]
+        self.left=self.left+diff*self.lp1[c]
         self.cnt[c]+=1
         self.cnt[self.last+c]+=1
         self.last=c
@@ -26,15 +31,25 @@ class Compress:
         if self.total1>1:
             self.total2+=1
 
+    #更新字典(TODO)
     def update(self):
-        self.
         pass
 
-mycompress=Compress()
-with open("./data.txt","r") as file:
-    for line in file:
-        for c in line:
-            mycompress.read(c)
+    #读取文件(TODO)
+    def readFile(self,inputFile):
+        with open("./"+inputFile,"r") as file:
+            for line in file:
+                for c in line:
+                    self.read(c)
 
-with open("./write.data","wb") as file:
-    file.write(bytes([10,2,3]))
+    #写文件(TODO)
+    def writeFile(self,outputFile):
+        with open("./"+outputFile,"wb") as file:
+            file.write("TODO")
+
+    #压缩(TODO)
+    def ziptxt(self,filename):
+        pass
+
+
+
