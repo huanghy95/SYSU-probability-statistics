bac0b049f14f9f2cb880d8652d06ba60f6341ca0
linn <673879891@qq.com>
2020-12-05 16:37:09 +0800

    读取&写入

--- ziptxt.py
+++ ziptxt.py
@@ -2,12 +2,13 @@ import compress
 import argparse
 
 #添加参数
-parser = argparse.ArgumentParser()
-parser.add_argument('inputFile', help='inputFile')
-parser.add_argument('outputFile', help='ouputFile')
-args=parser.parse_args()
-print(args.inputFile)
-print(args.outputFile)
-
-# mycompress=compress.Compress()
-# mycompress.ziptxt(inputFile,outputFile);
+# parser = argparse.ArgumentParser()
+# parser.add_argument('inputFile', help='inputFile')
+# parser.add_argument('outputFile', help='ouputFile')
+# args=parser.parse_args()
+# inputFile=args.inputFile
+# outputFile=args.outputFile
+inputFile="data.in"
+outputFile="data.out"
+mycompress=compress.Compress()
+mycompress.ziptxt(inputFile,outputFile)
