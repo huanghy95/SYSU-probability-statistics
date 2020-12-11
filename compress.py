5393fc149ae03fb171b0250718b176471fe5bda3
Lanly109 <1094916227@qq.com>
2020-12-11 22:56:35 +0800

    lanly_finish

--- compress.py
+++ compress.py
@@ -1,4 +1,5 @@
 import numpy as np
+import gmpy2
 import os
 import string
 import math
@@ -6,65 +7,97 @@ import utils
 class Compress:
     #初始化变量
     def __init__(self):
-        self.left=0.0#左区间
-        self.right=1.0#右区间
-        self.writeData=0#最大相同次数
-        self.last=-1#上一个字符的ascii码
+        gmpy2.get_context().precision = 1000
+        self.left=gmpy2.mpfr(0.0)#左区间
+        self.right=gmpy2.mpfr(1.0)#右区间
+        self.writeData=gmpy2.mpz(1)#最大相同次数
+        # self.last=-1#上一个字符的ascii码
         self.total=256
         self.cnt1=np.zeros(self.total)
-        self.cnt2=np.zeros(self.total*self.total)#字典
+        # self.cnt2=np.zeros(self.total*self.total)#字典
         #初始化字典(每个出现一次,方便计算概率)
         for i in range(self.total):
             self.cnt1[i]=1
-            for j in range(self.total):
-                self.cnt2[i*self.total+j]=1
+            # for j in range(self.total):
+            #     self.cnt2[i*self.total+j]=1
         #初始化一阶概率的左右端点
         self.lp1=np.zeros(self.total)
         self.rp1=np.zeros(self.total)
-        #初始化二阶概率的左右端点
-        self.lp2=np.zeros(self.total*self.total)
-        self.rp2=np.zeros(self.total*self.total)
-        utils.update(self.lp1,self.rp1,self.cnt1,self.total)
+        for i in range(self.total):
+            self.rp1[i] = (1 if i == 0 else self.rp1[i - 1] + self.cnt1[i])
+        tmp = self.rp1[self.total - 1]
+        self.rp1 /= tmp 
+        self.lp1[1:self.total] = self.rp1[0:self.total - 1]
+        # 不整二阶
+        # #初始化二阶概率的左右端点
+        # self.lp2=np.zeros(self.total*self.total)
+        # self.rp2=np.zeros(self.total*self.total)
+        # utils.update(self.lp1,self.rp1,self.cnt1,self.total)
+
+    def update_table(self, c):
+        self.cnt1[ord(c)] = self.cnt1[ord(c)] + 1
+        for i in range(self.total):
+            self.rp1[i] = (self.cnt1[0] if i == 0 else self.rp1[i - 1] + self.cnt1[i])
+        tmp = self.rp1[self.total - 1]
+        self.rp1 /= tmp 
+        self.lp1[1:self.total] = self.rp1[0:self.total - 1]
 
 
-    #计算可写入文件数字(TODO)
+    def read(self, c):
+        diff = self.right - self.left
+        self.right = self.left + diff * self.rp1[ord(c)]
+        self.left = self.left + diff * self.lp1[ord(c)]
+        self.update_table(c)
+
+
+    #计算可写入文件数字
     def generateWriteData(self):
-        # bits=0
-        # while True:
-        #     leftInt=int(self.left*10)
-        #     rightInt=int(self.right*10)
-        #     if leftInt==rightInt:
-        #         self.writeData*=10
-        #         self.writeData+=leftInt
-        #         self.left=self.left*10-leftInt
-        #         self.right=self.right*10-rightInt
-        #         bits+=1
-        #     else: 
-        #         self.writeData/=bits
-        #         return bits
-        pass
+        while True:
+            leftInt=int(self.left*10)
+            rightInt=int(self.right*10)
+            if leftInt==rightInt:
+                self.writeData*=10
+                self.writeData+=leftInt
+                self.left=self.left*10-leftInt
+                self.right=self.right*10-rightInt
+            else: 
+                break
 
-    #压缩(TODO)
+    #压缩
     def ziptxt(self,inputFile,outputFile):
         with open("./"+inputFile,"r") as readFile:
             with open("./"+outputFile,"wb") as writeFile:
-                pass
-        #         lines=0
-        #         for line in readFile:
-        #             for c in line:
-        #                 utils.read(self,c)
-        #                 bits=self.generateWriteData()
-        #                 if bits:
-        #                     # print(self.writeData)
-        #                     # print("write into file "+str(self.writeData)+" "+str(bytes(self.writeData.to_bytes(1,byteorder='big',signed=True))))
-        #                     writeFile.write(self.writeData.to_bytes(1,byteorder='big',signed=True))
-        #                     self.writeData=0
-        #             lines+=1
-        #             print("finish "+ str(lines) +" lines")
-        #         # while self.left:
-        #         #     leftInt=int(self.left*10)
-        #         #     self.left=self.left*10-leftInt
-        #         #     writeFile.write(leftInt.to_bytes(1,byteorder='big',signed=True))
-        # print("the size of the inputfile:"+str(os.path.getsize(inputFile))+" byte")
-        # print("the size of the outputfile:"+str(os.path.getsize(outputFile))+" byte")
+                lines=0
+                for line in readFile:
+                    for c in line:
+                        self.read(c)
+                        self.generateWriteData()
+                        # print(self.left)
+                        # print(self.right)
+                    lines+=1
+                self.writeData*=10
+                self.writeData+=int(self.right*10)
+                self.right=self.right*10-int(self.right*10)
+                # print(self.writeData.digits())
+                while(len(self.writeData.digits(2)) %8 != 0):
+                    self.writeData*=10
+                    self.writeData+=int(self.right*10)
+                    self.right=self.right*10-int(self.right*10)
+                # print(self.writeData.digits())
+                bit = self.writeData.digits(2)
+                # print(bit)
+                while len(bit) >= 8:
+                    qwq = int(bit[0:8],2)
+                    # print(qwq)
+                    writeFile.write(qwq.to_bytes(1, byteorder = 'big', signed = False));
+                    bit = bit[8:]
+                # print('********')
+                # print(len(bit))
+                # if len(bit) != 0:
+                #     qwq = int(bit, 2)
+                #     qwq <<= 8 - len(bit)
+                #     writeFile.write(qwq.to_bytes(1, byteorder = 'big', signed = False));
+
+        print("the size of the inputfile:"+str(os.path.getsize(inputFile))+" byte")
+        print("the size of the outputfile:"+str(os.path.getsize(outputFile))+" byte")
 
--- decompress.py
+++ decompress.py
@@ -1,4 +1,5 @@
 import numpy as np
+import gmpy2
 import os
 import string
 import math
@@ -6,31 +7,111 @@ import utils
 class Decompress:
     #初始化变量
     def __init__(self):
-        self.left=0.0#左区间
-        self.right=1.0#右区间
-        self.last=-1#上一个字符的ascii码
+        gmpy2.get_context().precision = 1000
+        self.left=gmpy2.mpfr(0.0)#左区间
+        self.right=gmpy2.mpfr(1.0)#右区间
+        self.readData=gmpy2.mpz(0)#最大相同次数
+        # self.last=-1#上一个字符的ascii码
         self.total=256
         self.cnt1=np.zeros(self.total)
-        self.cnt2=np.zeros(self.total*self.total)#字典
+        # self.cnt2=np.zeros(self.total*self.total)#字典
         #初始化字典(每个出现一次,方便计算概率)
         for i in range(self.total):
             self.cnt1[i]=1
-            for j in range(self.total):
-                self.cnt2[i*self.total+j]=1
+            # for j in range(self.total):
+            #     self.cnt2[i*self.total+j]=1
         #初始化一阶概率的左右端点
         self.lp1=np.zeros(self.total)
         self.rp1=np.zeros(self.total)
-        #初始化二阶概率的左右端点
-        self.lp2=np.zeros(self.total*self.total)
-        self.rp2=np.zeros(self.total*self.total)
-        utils.update(self.lp1,self.rp1,self.cnt1,self.total)
+        for i in range(self.total):
+            self.rp1[i] = (1 if i == 0 else self.rp1[i - 1] + self.cnt1[i])
+        tmp = self.rp1[self.total - 1]
+        self.rp1 /= tmp 
+        self.lp1[1:self.total] = self.rp1[0:self.total - 1]
+        # 不整二阶
+        # #初始化二阶概率的左右端点
+        # self.lp2=np.zeros(self.total*self.total)
+        # self.rp2=np.zeros(self.total*self.total)
+        # utils.update(self.lp1,self.rp1,self.cnt1,self.total)
 
-    #计算生成的字符
-    def generateChar(self):
-        pass
+    def update_table(self, c):
+        self.cnt1[ord(c)] = self.cnt1[ord(c)] + 1
+        for i in range(self.total):
+            self.rp1[i] = (self.cnt1[0] if i == 0 else self.rp1[i - 1] + self.cnt1[i])
+        tmp = self.rp1[self.total - 1]
+        self.rp1 /= tmp 
+        self.lp1[1:self.total] = self.rp1[0:self.total - 1]
 
-    #解压(TODO)
-    def ziptxt(self,inputFile,outputFile):
-        with open("./"+inputFile,"r") as readFile:
-            with open("./"+outputFile,"wb") as writeFile:
-                pass
+    def check(self, i):
+        diff = self.right - self.left
+        curleft = self.left + diff * self.lp1[i]
+        curright = self.left + diff * self.rp1[i]
+        qwq = self.cur / self.log10
+        qwq = qwq - gmpy2.floor(qwq)
+        # print(curleft)
+        # print(qwq)
+        # print(curright)
+        if curleft > qwq:
+            return False
+        if curright < qwq:
+            return False 
+        self.left = curleft
+        self.right = curright
+        return True
+
+    def update_cur(self):
+        while True:
+            leftInt=int(self.left*10)
+            rightInt=int(self.right*10)
+            if leftInt==rightInt:
+                self.left=self.left*10-leftInt
+                self.right=self.right*10-rightInt
+                self.cur *= 10
+                self.cur += ord(self.readData[0]) - 48
+                self.readData = self.readData[1:]
+            else: 
+                break
+
+    def get_c(self):
+        c = chr(49)
+        for i in range(self.total):
+            if self.check(i):
+                c = chr(i)
+                break
+        self.update_table(c)
+        self.update_cur()
+        # print(self.left)
+        # print(self.right)
+        return c
+
+    #解压
+    def unziptxt(self,inputFile,outputFile):
+        with open("./"+inputFile,"rb") as readFile:
+            while True:
+                i = readFile.read(1)
+                if not i:
+                    break
+                i = int.from_bytes(i, byteorder='big', signed=False)
+                # print(i)
+                self.readData *= 256
+                self.readData += i
+        self.readData = self.readData.digits(10)
+        # print(self.readData)
+        self.readData = self.readData[1:]#cut the first sign position
+        self.cur = gmpy2.mpfr(1.0)
+        self.log10 = gmpy2.mpfr(1.0)
+        for i in range(100):
+            self.log10 *= 10
+        for i in range(100):
+            self.cur *= 10
+            self.cur += ord(self.readData[0]) - 48
+            self.readData = self.readData[1:]
+        with open("./"+outputFile,"w") as writeFile:
+            while len(self.readData) != 0:
+                c = self.get_c()
+                writeFile.write(c)
+
+
+        print("the size of the inputfile:"+str(os.path.getsize(inputFile))+" byte")
+        print("the size of the outputfile:"+str(os.path.getsize(outputFile))+" byte")
+
