106a78a624f70b450fb4493e2b72d93c11031d4d
linn <673879891@qq.com>
2020-12-12 22:52:49 +0800

    static完工

--- unziptxt.py
+++ unziptxt.py
@@ -6,8 +6,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('inputFile', help='inputFile')
 parser.add_argument('outputFile', help='ouputFile')
 args=parser.parse_args()
-print(args.inputFile)
-print(args.outputFile)
+inputFile=args.inputFile
+outputFile=args.outputFile
 
 mydecompress=decompress.Decompress()
 mydecompress.unziptxt(inputFile,outputFile)
