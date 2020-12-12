ce2f4e0c0c942903caaf7703c32bababc51341fc
linn <673879891@qq.com>
2020-12-12 22:53:01 +0800

    static完工

--- unziptxt.py
+++ unziptxt.py
@@ -1,7 +1,7 @@
 import decompress 
 import argparse
 
-添加参数
+# 添加参数
 parser = argparse.ArgumentParser()
 parser.add_argument('inputFile', help='inputFile')
 parser.add_argument('outputFile', help='ouputFile')
